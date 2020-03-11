08dbfc72f10e52b91c77831fccfe13c7100be28d
Remych04 <remarenko-2010@yandex.ru>
2020-03-11 23:52:35 +0300

    init commit

---
+++ app/src/main/java/com/remych04/overeating/self/overeating/self/helping/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.remych04.overeating.self.overeating.self.helping
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
