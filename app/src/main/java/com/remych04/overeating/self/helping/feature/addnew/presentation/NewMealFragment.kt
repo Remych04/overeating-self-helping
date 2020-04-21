cfce456cec617856e9182cc54bf518b30940f47b
remych04 <remarenko-2010@yandex.ru>
2020-04-21 19:39:53 +0300

    binding to ext

--- app/src/main/java/com/remych04/overeating/self/helping/feature/addnew/presentation/NewMealFragment.kt
+++ app/src/main/java/com/remych04/overeating/self/helping/feature/addnew/presentation/NewMealFragment.kt
@@ -1,11 +1,10 @@
 package com.remych04.overeating.self.helping.feature.addnew.presentation
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.remych04.overeating.self.helping.R
 import com.remych04.overeating.self.helping.base.BaseFragment
+import com.remych04.overeating.self.helping.base.ext.binding
 import com.remych04.overeating.self.helping.data.MealDto
 import com.remych04.overeating.self.helping.databinding.NewMealFragmentBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -13,39 +12,26 @@ import java.util.*
 
 class NewMealFragment : BaseFragment(R.layout.daymeallist_fragment) {
 
-    private var _binding: NewMealFragmentBinding? = null
-    private val binding get() = _binding!!
+    private val bind by binding { NewMealFragmentBinding.bind(this.requireView()) }
+
     private val model by viewModel<NewMealViewModel>()
 
     override fun getViewModel() = model
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _binding = NewMealFragmentBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setTitle(resources.getString(R.string.new_meal_fragment_title))
-        binding.addNewMealButton.setOnClickListener {
+        bind.addNewMealButton.setOnClickListener {
             model.addNewMeal(
                 MealDto(
-                    meal = binding.mealEditText.text.toString(),
-                    feelings = binding.feelingsEditText.text.toString(),
+                    meal = bind.mealEditText.text.toString(),
+                    feelings = bind.feelingsEditText.text.toString(),
                     date = Calendar.getInstance().timeInMillis,
-                    unnecessary = binding.isUnnecessary.isChecked,
-                    replacement = binding.isReplacement.isChecked
+                    unnecessary = bind.isUnnecessary.isChecked,
+                    replacement = bind.isReplacement.isChecked
                 )
             )
         }
-    }
-
-    override fun onDestroyView() {
-        _binding = null
-        super.onDestroyView()
     }
 
     companion object {
